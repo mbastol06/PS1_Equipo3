96b8903486cdb0cbcdb77905e3c1e054844d742e
ldcarrilloa <ldcarrilloa@unal.edu.co>
2025-09-02 04:25:35 -0500

    Update codigo.R
    Web Scrapping 2

--- scripts/codigo.R
+++ scripts/codigo.R
@@ -18,7 +18,10 @@ pacman::p_load(
   xml2,
   tibble,
   purrr,
-  VIM
+  VIM,
+  gtsummary,
+  gt,
+  scales
 )
 
 ## ============================================================
@@ -37,3 +40,30 @@ fs::dir_create(dir_proc, recurse = TRUE)
 
 espera_js  <- 12   
 options(timeout = 180)
+
+## 2) Se extrae la primera tabla de cada página (la única con base en el tiempo de espera definido)
+
+extraer_primera_tabla <- function(b, url, espera_js = 12) {
+  b$Page$navigate(url)
+  b$Page$loadEventFired(wait_ = TRUE)
+  Sys.sleep(espera_js)
+  
+  
+## 3) Se utiliza el paquete Chromote para extraer la información renderizada de una fuente tipo Java Script y leerla en HTML
+  
+  doc      <- b$DOM$getDocument()
+  html_txt <- b$DOM$getOuterHTML(nodeId = doc$root$nodeId)[["outerHTML"]]
+  html_rnd <- xml2::read_html(html_txt)
+
+  nodos_tablas <- rvest::html_elements(html_rnd, "table")
+  message("   · Tablas detectadas: ", length(nodos_tablas))
+  if (length(nodos_tablas) == 0) return(NULL)
+
+  tb <- rvest::html_table(nodos_tablas[[1]], fill = TRUE)
+
+  tb <- tb |>
+    tibble::as_tibble(.name_repair = "unique") |>
+    janitor::clean_names()
+  
+  return(tb)
+}
