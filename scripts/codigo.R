afab6016e5cffcf77cddd3c789fdcbc3c4325739
ldcarrilloa <ldcarrilloa@unal.edu.co>
2025-09-02 01:48:12 -0500

    Update codigo.R
    Web Scrapping 1

--- scripts/codigo.R
+++ scripts/codigo.R
@@ -1,8 +1,39 @@
-# taller 1
+#### Taller 1 - Big Data and Machine Learning
 
-a <- 1
- a
- 
- 
- # web scaripn 
-  b <- 2
+### Punto 2 - Web Scrapping
+
+if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
+
+pacman::p_load(
+  tidyverse,
+  rvest,
+  httr,
+  fs,
+  janitor,
+  readr,
+  haven,
+  readxl,
+  glue,
+  chromote,
+  xml2,
+  tibble,
+  purrr,
+  VIM
+)
+
+## ============================================================
+## 1) Parámetros
+
+#Se carga la base original y se enumeran, del 1 al 10, las páginas subsiguientes para cada chunk
+
+url_base   <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/"
+paginas    <- tibble(chunk = 1:10,
+                     url   = sprintf("%spage%d.html", url_base, 1:10))
+
+dir_proc   <- "data/processed"
+fs::dir_create(dir_proc, recurse = TRUE)
+
+#Se dejan doce segundos de espera para cada una de las cargas de las páginas ya que tardan en cargar
+
+espera_js  <- 12   
+options(timeout = 180)
