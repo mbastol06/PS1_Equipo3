482310de886e2bbe8f86305d040c878ffbac7961
lucaseduardoveras <lucas_eduardo93@hotmail.com>
2025-09-04 20:44:44 -0500

    finished FWL

--- scripts/cuestion_4.R
+++ scripts/cuestion_4.R
@@ -7,6 +7,8 @@ setwd(dirname(dirname(rstudioapi::getActiveDocumentContext()$path)))
 require("pacman")
 pacman::p_load(rvest,tidyverse,stargazer)
 
+set.seed(1234)
+
 ################################################################################
 ################## Respondiendo la cuestión 4 ##################################
 ################################################################################
@@ -34,17 +36,49 @@ all_formal = lm(log(y_total_m) ~ female, data = data_reg)
 all_formal_h = lm(log(y_total_m_ha) ~ female, data = data_reg)
 
 stargazer(nominal_salary,princ_occup,all_occup,all_formal,all_formal_h, type = "text")
+stargazer(nominal_salary,princ_occup,all_occup,all_formal,all_formal_h, type = "latex",
+          out = "store/simple_female_reg.tex")
 
 ## Estimando usando Frisch Waugh-Lowell
 
 data_FWL = data %>% select(y_ingLab_m, female, age, age_sqr) %>% drop_na()
 
-reg_age = lm(data = data_reg, log(y_ingLab_m) ~ female + age + age_sqr)
+reg_age = lm(data = data_FWL, log(y_ingLab_m) ~ female + age + age_sqr)
 
-resid_ing = lm(data = data_reg, log(y_ingLab_m) ~ age + age_sqr)$resid
-resid_fem = lm(data = data_reg, female ~ age + age_sqr)$resid
+data_FWL <- data_FWL %>% mutate(resid_ing = lm(data = data_FWL, log(y_ingLab_m) ~ age + age_sqr)$resid) %>%
+mutate(resid_fem = lm(data = data_FWL, female ~ age + age_sqr)$resid)
 
 
-reg_age_FWL <- lm(resid_ing ~ resid_fem)
+reg_age_FWL <- lm(resid_ing ~ resid_fem, data = data_FWL)
 
-stargazer(reg_age, type = "text")
+stargazer(reg_age, reg_age_FWL, type = "text")
+
+# Bootstrap with B = 5000
+
+B = 5000
+
+coefs_store = rep(NA,B)
+
+for(i in 1:B){
+  
+  FWL_sample<- sample_frac(data_FWL,size=1,replace=TRUE) #takes a sample with replacement of the same size of the original sample (1 or 100%)
+  
+  FWL_sample <- FWL_sample %>% mutate(resid_ing = lm(data = FWL_sample, log(y_ingLab_m) ~ age + age_sqr)$resid) %>%
+    mutate(resid_fem = lm(data = FWL_sample, female ~ age + age_sqr)$resid)
+  reg_age_FWL_sample <- lm(resid_ing ~ resid_fem, data = FWL_sample)
+  
+  coefs<-reg_age_FWL_sample$coefficients[2] # gets the coefficient of interest that coincides with the elasticity of demand
+  
+  coefs_store[i]<-coefs #saves it in the above vector
+}
+
+sd <- sd(coefs_store)
+print(sd)
+
+sd_hist <- ggplot(data = data.frame(sd = coefs_store), aes(x = sd)) + geom_histogram()
+
+
+write.csv(sd, "stores/sd_FWL.csv")
+stargazer(reg_age, reg_age_FWL, type = "latex", out = "stores/reg_FWL.tex")
+ggsave(sd_hist, dpi = 300,filename = "stores/sd_fwl_hist.png")
+#-------------------------------------------------------------------------------
